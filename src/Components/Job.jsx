05d93f887b696516f784dc1cf09fd4d941b82cc8
Stephen Oryema <148610430+OryemaStephen@users.noreply.github.com>
2024-07-06 23:01:51 +0300

    Updated job details

--- src/Components/Job.jsx
+++ src/Components/Job.jsx
@@ -1,7 +1,48 @@
-const Job = () => {
-  return (
-    <div>Job</div>
-  )
-}
+import { PropTypes } from "prop-types";
 
-export default Job
+const Job = ({ jobs }) => {
+  return (
+    <>
+        {jobs.map((job) => (
+          <div className="job" key={job.id}>
+            <div className="job-descrition">
+              <img className="logo" src={job.logo} alt={job.company}/>
+              <div className="desc">
+                <div className="title">
+                  <span>{job.company}</span>
+                  <span>{job.new?'New':''}</span>
+                  <span>{job.featured?'Featured':''}</span>
+                </div>
+                <div className="position">
+                  {job.position}
+                </div>
+                <div className="extra-details">
+                  <span>{job.postedAt}</span>
+                  <span>{job.contract}</span>
+                  <span>{job.location}</span>
+                </div>
+              </div>
+            </div>
+            <div className="skills">
+              <span>{job.role}</span>
+              <span>{job.level}</span>
+              <span>{job.languages}</span>
+              <span>{job.tool}</span>
+            </div>
+          </div>
+        ))}
+    </>
+  );
+};
+
+Job.propTypes = {
+  jobs: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+    })
+  ).isRequired,
+};
+
+export default Job;
--- src/Components/JobWrapper.jsx
+++ src/Components/JobWrapper.jsx
@@ -1,13 +1,24 @@
+import { useEffect, useState } from "react";
 import Job from "./Job";
-import Skills from "./Skills";
+
 
 const JobWrapper = () => {
+    const [jobs, setJobs] = useState([])
+
+    useEffect(()=>{
+        fetch('/data.json')
+        .then((response)=>response.json())
+        .then((data)=>{
+          setJobs(data);
+        })
+        .catch((error)=>console.log(error))
+    },[]);
+
   return (
-    <div className="job">
-        <Job />
-        <Skills />
+    <div className="jobs">
+        <Job jobs={jobs} />
     </div>
   )
 }
 
-export default JobWrapper
+export default JobWrapper;
